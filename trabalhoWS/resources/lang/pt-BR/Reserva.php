7cd0e26da19772ef50bd186270a255cdf8d2070c
unknown <robson_herculano@hotmail.com>
2023-06-18 09:40:25 -0300

    ajustesReserva

---
+++ trabalhoWS/resources/lang/pt-BR/Reserva.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    "listreturn" => "Lista de reservas retornada",
+    "created" => "Reserva efetuada",
+    "returned" => "Dados da reserva",
+    "updated" => "Reserva atualizada com sucesso",
+    "deleted" => "Reserva apagada"
+];
